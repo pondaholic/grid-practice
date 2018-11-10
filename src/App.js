cf010b841a286598c8ac4b82eed9697c8b23cf67
Shelly Leung <pontificable@yahoo.com>
2018-11-09 17:33:32 -0800

    grid practice

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@ import './App.css';
 class App extends Component {
 	render() {
 		return (
-			<div className="site-border">
-				<div className="site-body">
-					<div className="container">container</div>
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
+			<div className="site-body">
+				<div className="header">header</div>
+				<div className="body">
+					body
+					<div className="navbar">nav</div>
 				</div>
+				<div className="footer">footer</div>
 			</div>
+
+			// <div className="site-border">
+			// 	<div className="site-body">
+			// 		<div className="container">container</div>
+			// 		<img src={logo} className="App-logo" alt="logo" />
+			// 		<p>
+			// 			Edit <code>src/App.js</code> and save to reload.
+			// 		</p>
+			// 		<a
+			// 			className="App-link"
+			// 			href="https://reactjs.org"
+			// 			target="_blank"
+			// 			rel="noopener noreferrer"
+			// 		>
+			// 			Learn React
+			// 		</a>
+			// 	</div>
+			// </div>
 		);
 	}
 }
