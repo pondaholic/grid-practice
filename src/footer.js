719233b524a779d795c819e3215f468fe9af63dd
Shelly Leung <pontificable@yahoo.com>
2018-11-23 16:39:13 -0800

    background image changed for portfolio page

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './footer.css';
 
-export default function Footer(props) {
+export default function Footer() {
 	return (
 		<div className="footer">
 			<p>Created by: Shelly</p>
