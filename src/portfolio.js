610ee4f22faf4758df95fb71a5eea92f314838a4
Shelly Leung <pontificable@yahoo.com>
2018-11-23 16:23:39 -0800

    3 apps added

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import mtg from './app-images/mtg.png';
 import podquest from './app-images/podquest.png';
+import morse from './app-images/morse.png';
+
 import './portfolio.css';
 
 export default class Portfolio extends React.Component {
@@ -42,7 +44,24 @@ export default class Portfolio extends React.Component {
 						</p>
 					</a>
 				</div>
-				<div className="project-3">Another App</div>
+				<div className="project-3">
+					<h3>Learn Morse Code App</h3>
+					<p>A description of the app</p>
+					<p>What I did for the app: </p>
+					<a href="https://github.com/thinkful-ei23/SRC-Morse-Client">
+						<p>
+							<i>Morse Code's Github</i>
+						</p>
+					</a>
+					<a href="https://morse-learning-app.herokuapp.com">
+						<img src={morse} alt="screenshot of Morse Code questions" />
+					</a>
+					<a href="https://github.com/thinkful-ei23/SRC-Morse-Server">
+						<p>
+							<i>Link to server-side code</i>
+						</p>
+					</a>
+				</div>
 			</div>
 		);
 	}
